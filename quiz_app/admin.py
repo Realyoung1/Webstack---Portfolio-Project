cefff9f337d1baa3efe4136aea9f6df5c9478925
Ajewole Tope Ebenezer <olalekano47@gmail.com>
2024-03-21 02:45:10 -0700

    Admin Authentication and Quiz functionalities are added

--- quiz_app/admin.py
+++ quiz_app/admin.py
@@ -1,16 +1,25 @@
-
-# Register your models here.
-from quiz_app.models import Quiz, Question, AnswerOption
 from django.contrib import admin
+from .models import CustomUser, Quiz, Question, AnswerOption, QuizAttempt, UserAnswer
 
+# Optional: Customize how models are displayed in the admin
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 3  # Number of extra blank questions to show
 
 class AnswerOptionInline(admin.TabularInline):
     model = AnswerOption
-    extra = 3  # Determines the number of empty forms to display
+    extra = 4  # Number of extra blank answers to show
+
+class QuizAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline]  # This allows you to add questions directly from the quiz form
 
 class QuestionAdmin(admin.ModelAdmin):
-    inlines = [AnswerOptionInline]
+    inlines = [AnswerOptionInline]  # This allows you to add answer options directly from the question form
 
-admin.site.register(Quiz)
+# Register your models here.
+admin.site.register(CustomUser)
+admin.site.register(Quiz, QuizAdmin)
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(AnswerOption)
+admin.site.register(QuizAttempt)
+admin.site.register(UserAnswer)
